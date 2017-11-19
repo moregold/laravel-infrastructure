ad5725efcde0fcfa8760e60653118b95adc93e18
RoyWang <yuelongnet@163.com>
2017-11-20 06:39:55 +0800

    add a new function

--- src/Helpers/SubAttributesTrait.php
+++ src/Helpers/SubAttributesTrait.php
@@ -8,7 +8,23 @@ trait SubAttributesTrait{
 		$value = $this->_attribute_relation_value_name;
 		$attribute_value = $this->$realtion_name->where($this->_attribute_relation_key_name, $name)->first();
 		return $attribute_value ? $attribute_value->$value : false;
+	}
 
+	/**
+	 *
+	 * Get sub attribute value, and create a new record if there is no record
+	 */
+	public function getSubAttributeWithDefault($attribute_name = '', $default_value = '')
+	{
+		if( !$result = $this->getSubAttribute( $attribute_name ) ) {
+			$realtion_name = $this->_attribute_relation_name;
+			$this->$realtion_name()->create([
+				$this->_attribute_relation_key_name   => $attribute_name,
+				$this->_attribute_relation_value_name => $default_value,
+			]);
+			$result = $default_value;
+		}
+		return $result;
 	}
 
 }
